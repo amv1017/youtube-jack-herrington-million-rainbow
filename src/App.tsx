6f0ad195d37b9a43cbe2cf29e013698aded659b8
Alexander <dr.ambercat@gmail.com>
2023-06-28 12:46:31 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+// @ts-ignore
+import rainbowGradient from 'rainbow-gradient'
+
+type ColorProps = { colors: string[] }
+
+function ReactColors({ colors }: ColorProps) {
+  const minWidth = `${100.0 / colors.length}vw`
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
+      {colors.map((color, index) => (
+        <div
+          key={[color, index].join('-')}
+          style={{
+            minHeight: '100vh',
+            minWidth,
+            backgroundColor: color,
+          }}
+        ></div>
+      ))}
+    </div>
+  )
+}
+
+const rainbowColors = rainbowGradient(360).map(
+  ([r, g, b]: any) => `rgb(${r},${g},${b})`
+)
+
+const App = () => {
+  const [colors, setColors] = useState(
+    new Array(360 * 4)
+      .fill(0)
+      .map((_, i) => rainbowColors[i % rainbowColors.length])
+  )
+
+  useEffect(() => {
+    let mounted = true
+    const update = () => {
+      if (mounted) {
+        setColors((colors) => {
+          const newColors = [...colors]
+          newColors.push(newColors.shift() as string)
+          return newColors
+        })
+        requestAnimationFrame(update)
+      }
+    }
+    requestAnimationFrame(update)
+    return () => {
+      mounted = false
+    }
+  }, [])
+
+  return <ReactColors colors={colors} />
+}
+
+export default App
